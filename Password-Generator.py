34bfe14c86dd1be4184e9c8f825f3a3a522ed9c2
UserDevz <98434149+UserDevz@users.noreply.github.com>
2022-03-03 20:08:41 -0400

    Add files via upload

---
+++ Password-Generator.py
@@ -0,0 +1,22 @@
+# Criador: UserDevz
+# Gerador de senhas fortes!
+# modo de uso:
+#    •python Password-Generator.py <numero de caracteres>
+
+# importando as bibliotecas necessarias:
+import random
+
+# Definindo os caracteres para a senha:
+letras = "abcdefghijklmnopqrstuvwxyz"
+numero = "0123456789"
+caracteres = "[]{};:*'+-&_$#@()?!"
+letras_A = letras.upper()
+
+# Tamanho da senha desejada:
+tamanho = 16
+
+# Gerando a senha:
+geral = letras + numero + caracteres + letras_A
+password = "".join(random.sample(geral, tamanho))
+
+print(f'Senha gerada: {password}\n')
